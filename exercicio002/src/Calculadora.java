00b19f027c28f87e9fac89518984a89441ba43c9
Raphael Gomes <raphael18pro@gmail.com>
2024-02-23 20:05:29 -0300

    iniciando exercício 3

---
+++ exercicio002/src/Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+    public double ad(double x, double y) {
+        return x + y;
+    }
+
+    public double sub(double x, double y) {
+        return x - y;
+    }
+
+    public double mult(double x, double y) {
+        return x * y;
+    }
+
+    public double div(double x, double y) {
+        return x / y;
+    }
+}
---
+++ exercicio002/src/End.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class End {
+    Calculadora calculadora = new Calculadora();
+    Start inicio = new Start();
+    Scanner ler = new Scanner(System.in);
+
+
+    public static int fim() {
+
+        System.out.println("O resultado da sua operação é: ");
+        System.out.println();
+    }
+}
