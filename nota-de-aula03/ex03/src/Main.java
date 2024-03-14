de6e924944b257de35e3ed641c46067855ca01eb
Raphael Vieira Gomes <Rvieira@academicos.com>
2024-03-14 20:35:41 -0300

    terminando nota de aula 3

--- nota-de-aula03/ex03/src/Main.java
+++ nota-de-aula03/ex03/src/Main.java
@@ -7,11 +7,14 @@ public class Main {
         // Contas
         ContaCorrente c = new ContaCorrente();
         ContaPoupanca p = new ContaPoupanca();
-        // Artifícios do switchCase
+        // Escolha para o switchCase
         int escolha;
-        int escolhaFiltrada;
-
-        double deposito;
+        // Artifícios do switchCase para conta Corrente;
+        int escolhaFiltradaCorrente;
+        double depositoCorrente;
+        // Artifícios do switchCase para conta Poupança
+        int escolhaFiltradaPoupanca;
+        double depositoPoupanca;
 
         System.out.println("Olá, seja bem-vindo(a), qual é o seu nome ?");
         String nome = ler.next();
@@ -22,20 +25,20 @@ public class Main {
         do {
             System.out.println("Selecione o tipo de conta:\n1. Corrente\n2.Poupança");
             escolha = ler.nextInt();
-    
+
             switch (escolha) {
                 case 1:
                     System.out.println("O que deseja fazer ?\n1. Depositar\n2.Sacar\n3. Usar cheque especial\n4. Exibir dados da conta");
-                    escolhaFiltrada = ler.nextInt();
-    
-                    switch (escolhaFiltrada) {
+                    escolhaFiltradaCorrente = ler.nextInt();
+
+                    switch (escolhaFiltradaCorrente) {
                         case 1:
                             System.out.println("Insira a quantia que deseja depositar:");
-                            deposito = ler.nextDouble();
-                            c.setSaldo(c.getSaldo() + deposito);
+                            depositoCorrente = ler.nextDouble();
+                            c.setSaldo(c.getSaldo() + depositoCorrente);
                             System.out.println("Depósito realizado com sucesso !");
                             break;
-    
+
                         case 2:
                             System.out.println("Insira a quantia que deseja sacar:");
                             double quantia = c.sacar(ler.nextInt());
@@ -47,11 +50,11 @@ public class Main {
                                 System.out.println("Não foi possível realizar o saque, você não tem saldo suficiente");
                                 break;
                             }
-    
+
                         case 3:
                             System.out.println("Quanto de cheque especial você deseja ter acesso ?");
                             double concederChequeEspecial = ler.nextDouble();
-    
+
                             if (concederChequeEspecial <= c.chequeEspecial) {
                                 System.out.println("Valor concedido");
                                 break;
@@ -59,15 +62,47 @@ public class Main {
                                 System.out.println("Valor negado, seu acesso ao cheque especial atualmente é de R$" + c.chequeEspecial);
                                 break;
                             }
-                        
+
                         case 4:
-                        System.out.println(c.exibirDados());
-    
+                            System.out.println(c.exibirDados());
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("O que deseja fazer ?\n1. Depositar\n2.Sacar\n3. Calcular rendimentos\n4. Exibir dados da conta");
+                    escolhaFiltradaPoupanca = ler.nextInt();
+
+                    switch (escolhaFiltradaPoupanca) {
+                        case 1:
+                            System.out.println("Insira a quantia que deseja depositar:");
+                            depositoPoupanca = ler.nextDouble();
+                            p.setSaldo(p.getSaldo() + depositoPoupanca);
+                            System.out.println("Depósito realizado com sucesso !");
+                            break;
+
+                        case 2:
+                            System.out.println("Insira a quantia que deseja sacar:");
+                            double quantiaPoupanca = p.sacar(ler.nextInt());
+                            if (p.getSaldo() >= quantiaPoupanca) {
+                                p.setSaldo(p.getSaldo() - quantiaPoupanca);
+                                System.out.println("Saque realizado com sucesso !");
+                                break;
+                            } else {
+                                System.out.println("Não foi possível realizar o saque, você não tem saldo suficiente");
+                                break;
+                            }
+
+                        case 3:
+                            System.out.println(p.calcularRendimento());
+                            break;
+                        case 4:
+                            System.out.println(p.exibirDados());
                     }
             }
             System.out.println("Deseja repetir ? (s/n)");
             repetir = ler.next();
-        } while (repetir.equalsIgnoreCase("s"));
+        }  while (repetir.equalsIgnoreCase("s")) ;
     }
 }
 
